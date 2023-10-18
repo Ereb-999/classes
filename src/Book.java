c1b3c9ccedde0a6fc27f36a96a7236d09959bd6f
Ereb-999 <Naofumi999@yandex.ru>
2023-10-18 22:04:45 +0400

    classes chapter the end2

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
 public class Book {
     private String bookName;
     private int age;
+    private Author author;
     public Book(String bookName, int age) {
         this.bookName = bookName;
         this.age = age;
+        this.author = getAuthor();
     }
     public String getBookName(){
         return this.bookName;
@@ -12,7 +14,13 @@ public class Book {
         return this.age;
     }
 
+    public Author getAuthor() {
+        return author;
+    }
+
     public void setAge (int age) {
         this.age = age;
     }
+
+
 }
