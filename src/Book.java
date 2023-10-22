6ff60aa53ba365378420962e94ef05e437f28d0f
Ereb-999 <Naofumi999@yandex.ru>
2023-10-22 13:23:52 +0400

    ojeckt chapter the end

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 public class Book {
-    private String bookName;
+    private final String bookName;
     private int age;
-    private Author author;
+    private final Author author;
     public Book(String bookName, int age, Author author) {
         this.bookName = bookName;
         this.age = age;
@@ -15,12 +17,27 @@ public class Book {
     }
 
     public Author getAuthor() {
-        return author;
+        return this.author;
     }
 
     public void setAge (int age) {
         this.age = age;
     }
+    @Override
+    public String toString(){
+        return "Книга " + bookName + ". Опубликованая " + age + " год, авторство " + author + ".";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return age == book.age && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, age);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Author gov = new Author("Говард", "Лафкрафт");
         Author lev = new Author("Лев", "Толстой");
-       Book necro = new Book("Некронамикон", 1938, gov);
+        Book necro = new Book("Некронамикон", 1938, gov);
         Book world = new Book("Война и мир", 1863, lev);
-       System.out.println("Книга: " + necro.getBookName());
-        System.out.println("Автор: " + gov.getAuthorName() + " " + gov.getSurnameName());
-       System.out.println("Год публикации: " + necro.getAge());
-       System.out.println(" ");
-        System.out.println("Книга: " + world.getBookName());
-        System.out.println("Автор: " + lev.getAuthorName() + " " + lev.getSurnameName());
-        System.out.println("Год публикации: " + world.getAge());
-        System.out.println(" ");
+        System.out.println(gov);
+        System.out.println(lev);
+        System.out.println(necro);
+        System.out.println(world);
         world.setAge(1870);
-        System.out.println("Книга: " + world.getBookName());
-        System.out.println("Автор: " + lev.getAuthorName() + " " + lev.getSurnameName());
-        System.out.println("Год публикации: " + world.getAge());
+        System.out.println(world);
     }
 }
